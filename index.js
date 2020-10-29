bcc9a0628386b08ec5f348a454c8315db39869ea
rschpdr <pedro.resch@gmail.com>
2020-10-29 17:59:34 -0300

    done

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const btnStart = document.getElementById("startGameBtn");
 const canvas = document.getElementById("theCanvas");
 const ctx = canvas.getContext("2d");
 
+const img = new Image();
+img.src = "./gameOver.jpg";
+
 const gameArea = {
   player: null,
+  obstacles: [],
+  frames: 0,
+  score: 0,
+  animationId: 0,
 };
 
 // Start the game
@@ -23,7 +30,71 @@ function updateGame() {
   gameArea.player.newPos();
   gameArea.player.update();
 
-  requestAnimationFrame(updateGame);
+  updateObstacles();
+
+  updateScore(gameArea.score);
+
+  gameArea.animationId = requestAnimationFrame(updateGame);
+
+  checkGameOver();
+}
+
+function updateScore(score) {
+  ctx.fillStyle = "red";
+  ctx.font = "20px Arial";
+
+  ctx.fillText(`Score: ${score}`, canvas.width - 90, 40);
+}
+
+function checkGameOver() {
+  const crashed = gameArea.obstacles.some((obstacle) => {
+    return gameArea.player.isCrashedWith(obstacle);
+  });
+
+  if (crashed) {
+    cancelAnimationFrame(gameArea.animationId);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  }
+}
+
+function updateObstacles() {
+  gameArea.frames++;
+
+  if (gameArea.frames % 30 === 0) {
+    gameArea.score++;
+  }
+
+  gameArea.obstacles.map((obstacle) => {
+    obstacle.x--;
+    obstacle.update();
+  });
+
+  if (gameArea.frames % 120 === 0) {
+    let x = canvas.width;
+    let minHeight = 20;
+    let maxHeight = 200;
+    let height = Math.floor(
+      Math.random() * (maxHeight - minHeight + 1) + minHeight
+    );
+
+    let minGap = 50;
+    let maxGap = 200;
+    let gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
+
+    const upperObstacle = new Component(x, 0, 10, height, "green");
+    const bottomObstacle = new Component(
+      x,
+      height + gap,
+      10,
+      x - height - gap,
+      "green"
+    );
+
+    gameArea.obstacles.push(upperObstacle);
+    gameArea.obstacles.push(bottomObstacle);
+  }
 }
 
 class Component {
@@ -46,6 +117,33 @@ class Component {
     this.x += this.speedX;
     this.y += this.speedY;
   }
+
+  left() {
+    return this.x;
+  }
+
+  right() {
+    return this.x + this.width;
+  }
+
+  top() {
+    return this.y;
+  }
+
+  bottom() {
+    return this.y + this.height;
+  }
+
+  isCrashedWith(obstacle) {
+    const condition = !(
+      this.bottom() < obstacle.top() ||
+      this.top() > obstacle.bottom() ||
+      this.right() < obstacle.left() ||
+      this.left() > obstacle.right()
+    );
+
+    return condition;
+  }
 }
 
 // Espera a página carregar tudo
@@ -53,8 +151,6 @@ window.addEventListener("load", () => {
   btnStart.addEventListener("click", startGame);
 
   document.addEventListener("keydown", (event) => {
-    console.log(event.key);
-
     switch (event.key) {
       case "ArrowLeft":
         gameArea.player.speedX -= 1;
@@ -74,8 +170,6 @@ window.addEventListener("load", () => {
   });
 
   document.addEventListener("keyup", (event) => {
-    console.log(event.key);
-
     gameArea.player.speedX = 0;
     gameArea.player.speedY = 0;
   });
